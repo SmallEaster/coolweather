0cb08af423f519ff054459cf35375ad9c0b18ae0
Easter <284782880@qq.com>
2016-05-03 19:11:10 +0800

    显示天气信息

--- src/com/example/coolweather/activity/ChooseAreaActivity.java
+++ src/com/example/coolweather/activity/ChooseAreaActivity.java
@@ -17,7 +17,10 @@ import com.example.coolweather.util.Utility;
 import android.R;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -49,10 +52,20 @@ public class ChooseAreaActivity extends Activity{
 	private Province selectedProvince;
 	private City selectedCity;
 	private int currentLevel;
+	private boolean isFromWeatherActivity;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		isFromWeatherActivity=getIntent().getBooleanExtra("from_weather_activity", false);
+		SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
+		if (prefs.getBoolean("city_selected", false)&&!isFromWeatherActivity) {
+			Intent intent = new Intent(this, WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(com.example.coolweather.R.layout.choose_area);
 		
@@ -73,6 +86,12 @@ public class ChooseAreaActivity extends Activity{
 				}else if (currentLevel==LEVEL_CITY) {
 					selectedCity = cityList.get(position);
 					queryCounties();
+				}else if (currentLevel==LEVEL_COUNTY) {
+					String countyCode=countyList.get(position).getCountyCode();
+					Intent intent=new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 
@@ -116,7 +135,7 @@ public class ChooseAreaActivity extends Activity{
 		}
 	}
 	
-	private void queryCounties() {
+	public void queryCounties() {
 		// TODO Auto-generated method stub
 		countyList=coolWeatherDB.loadCounties(selectedCity.getId());
 		if (countyList.size()>0) {
@@ -214,6 +233,10 @@ public class ChooseAreaActivity extends Activity{
 		else if (currentLevel==LEVEL_CITY) {
 			queryProvinces();
 		}else {
+			if (isFromWeatherActivity) {
+				Intent intent = new Intent(this, WeatherActivity.class);
+				startActivity(intent);
+				}
 			finish();
 		}
 	}	
--- src/com/example/coolweather/util/Utility.java
+++ src/com/example/coolweather/util/Utility.java
@@ -1,6 +1,12 @@
 package com.example.coolweather.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.example.coolweather.db.CoolWeatherDB;
 import com.example.coolweather.model.City;
@@ -9,6 +15,9 @@ import com.example.coolweather.model.Province;
 
 
 import android.R.string;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 public class Utility {
@@ -73,5 +82,44 @@ public class Utility {
 				}
 			}
 		return false;
+	}
+	
+	//解析天气信息
+	public static boolean handleWeatherResponse(Context context,String response)
+	{
+		JSONObject jasonObject;
+		try {
+			jasonObject = new JSONObject(response);
+			JSONObject weatherInfo=jasonObject.getJSONObject("weatherinfo");
+			String cityName=weatherInfo.getString("city");
+			String weatherCode=weatherInfo.getString("cityid");
+			String temp1=weatherInfo.getString("temp1");
+			String temp2=weatherInfo.getString("temp2");
+			String weatherDesp=weatherInfo.getString("weather");
+			String publishTime=weatherInfo.getString("ptime");
+			saveWeatherInfo(context,cityName,weatherCode,temp1,temp2,weatherDesp,publishTime);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	private static void saveWeatherInfo(Context context, String cityName,
+			String weatherCode, String temp1, String temp2, String weatherDesp,
+			String publishTime) {
+		// TODO Auto-generated method stub
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+		SharedPreferences.Editor editor=PreferenceManager.getDefaultSharedPreferences(context).edit();
+		
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));
+		editor.commit();
 	}
 }
